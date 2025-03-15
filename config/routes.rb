dae612eec34f8d08cbb90b0d94c0fba1e5651c8a
spheal363 <115055083+spheal363@users.noreply.github.com>
2025-03-15 14:08:35 +0900

    fix: 投稿フォームのデータをControllerでデータベースに書き込む

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :articles
-  root 'welcome#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
