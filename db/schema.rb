2fb69e197a1eb18e3887196e0c725379e52553bc
spheal363 <115055083+spheal363@users.noreply.github.com>
2025-03-15 13:34:42 +0900

    feature: Feelingカラムを追加し、表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20250314022709) do
+ActiveRecord::Schema.define(version: 20250315043100) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.string   "feeling"
   end
 
 end
